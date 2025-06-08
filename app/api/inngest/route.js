99acba6ab1785da15ec464fbb8815db2a8de0833
prabath-manohara <prabathlakshitha1237@gmail.com>
2025-06-08 23:27:11 +0530

    Add Inngest serve handler and fix paths

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,57 +1,18 @@
-import { Inngest } from "inngest";
-import connectDB from "../../../config/db";
-import User from "@/models/User";
+// app/api/inngest/route.ts
 
-// Create a client
-export const inngest = new Inngest({ id: "quickcart-next" });
+import { serve } from "inngest/next";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+} from "./inngest"; // 👈 this should match the file where your inngest functions are defined
 
-// Sync user creation
-export const syncUserCreation = inngest.createFunction(
-  {
-    id: "sync-user-from-clerk",
-  },
-  { event: "clerk/user.created" },
-  async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data;
-    const userData = {
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
-      imageUrl: image_url,
-    };
-    await connectDB();
-    await User.create(userData);
-  }
-);
-
-// Sync user update
-export const syncUserUpdation = inngest.createFunction(
-  {
-    id: "update-user-from-clerk",
-  },
-  { event: "clerk/user.updated" },
-  async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data;
-    const userData = {
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
-      imageUrl: image_url,
-    };
-    await connectDB();
-    await User.findByIdAndUpdate(id, userData);
-  }
-);
-
-// Sync user deletion
-export const syncUserDeletion = inngest.createFunction(
-  {
-    id: "delete-user-from-clerk",
-  },
-  { event: "clerk/user.deleted" },
-  async ({ event }) => {
-    const { id } = event.data;
-    await connectDB();
-    await User.findByIdAndDelete(id);
-  }
-);
+export const { GET, POST } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});
